64191762c07d1aebe3d0ff45df87784c2bcae8ff
PatrickDenis-stack <77415730+PatrickDenis-stack@users.noreply.github.com>
2023-08-08 16:13:44 +0200

    fix: enable empty to_time for firstly inserted doc

--- arbeitszeiterfassung_s4a/arbeitszeiterfassung_s4a/doctype/working_time/working_time.js
+++ arbeitszeiterfassung_s4a/arbeitszeiterfassung_s4a/doctype/working_time/working_time.js
@@ -24,13 +24,21 @@ frappe.ui.form.on("Working Time", {
 
 frappe.ui.form.on("Working Time Log", {
 	time_logs_add: function (frm, cdt, cdn) {
-		let prev_row = frm.doc.time_logs.at(-2);
-		if (frm.doc.time_logs.length > 1) {
+		let current_row = locals[cdt][cdn];
+		let index = frm.doc.time_logs.findIndex(row => row.name === current_row.name);
+		
+		if (index > 0) {
+			let prev_row = frm.doc.time_logs[index - 1];
+			
 			if (!prev_row.to_time || prev_row.to_time.trim() === "") {
 				frappe.model.set_value(cdt, prev_row.name, "to_time", frappe.datetime.now_time(false));
 			}
+			
+			frappe.model.set_value(cdt, cdn, "from_time", prev_row.to_time || frappe.datetime.now_time(false));
+		} else {
+			frappe.model.set_value(cdt, cdn, "from_time", frappe.datetime.now_time(false));
 		}
-		frappe.model.set_value(cdt, cdn, "from_time", prev_row.to_time || frappe.datetime.now_time(false));
-		frappe.model.set_value(cdt, cdn, "to_time", "");  // Otherwise the framework may overwrite empty values with the current time on save.
+		
+		frappe.model.set_value(cdt, cdn, "to_time", "");  // Otherwise, the framework may overwrite empty values with the current time on save.
 	},
 });
